2b2320250eb03410ed5d1203a8d9370213fd2090
anik <anik@w3engineers.com>
2020-07-25 00:45:19 +0600

    # Feature   - mediaSoup react part complete

---
+++ src/components/JoinDialog.js
@@ -0,0 +1,294 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import { withRoomContext } from '../RoomContext';
+import isElectron from 'is-electron';
+import * as settingsActions from '../actions/settingsActions';
+import PropTypes from 'prop-types';
+import { useIntl, FormattedMessage } from 'react-intl';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import CookieConsent from 'react-cookie-consent';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import Grid from '@material-ui/core/Grid';
+import logo from '../images/wire2-logo.png';
+
+const styles = (theme) => ({
+  root: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  dialogTitle: {},
+  dialogPaper: {
+    width: '30vw',
+    padding: theme.spacing(2),
+    [theme.breakpoints.down('lg')]: {
+      width: '40vw',
+    },
+    [theme.breakpoints.down('md')]: {
+      width: '50vw',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '70vw',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '90vw',
+    },
+  },
+  logo: {
+    display: 'block',
+    paddingBottom: '1vh',
+  },
+  loginButton: {
+    position: 'absolute',
+    right: theme.spacing(2),
+    top: theme.spacing(2),
+    padding: 0,
+  },
+  largeIcon: {
+    fontSize: '2em',
+  },
+  largeAvatar: {
+    width: 50,
+    height: 50,
+  },
+  green: {
+    color: 'rgba(0, 153, 0, 1)',
+  },
+  red: {
+    color: 'rgba(153, 0, 0, 1)',
+  },
+  siteLogo: {
+    width: '30%',
+    display: 'block',
+  },
+  hrS: {
+    margin: '10px 0;',
+  },
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+const JoinDialog = ({
+  roomClient,
+  room,
+  roomId,
+  displayName,
+  displayNameInProgress,
+  loggedIn,
+  myPicture,
+  changeDisplayName,
+  classes,
+}) => {
+  const intl = useIntl();
+
+  const handleKeyDown = (event) => {
+    const { key } = event;
+
+    switch (key) {
+      case 'Enter':
+      case 'Escape': {
+        if (displayName === '') changeDisplayName('Guest');
+        if (room.inLobby) roomClient.changeDisplayName(displayName);
+        break;
+      }
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <Dialog
+        open
+        classes={{
+          paper: classes.dialogPaper,
+        }}
+      >
+        <Grid container direction="row" justify="center" alignItems="center">
+          <img alt="wire logo" variant="square" src={logo} className={classes.siteLogo}></img>
+        </Grid>
+        <DialogContent>
+          <DialogContentText gutterBottom align="center">
+            <FormattedMessage id="room.aboutToJoin" defaultMessage="You are about to join a meeting" />
+          </DialogContentText>
+
+          <DialogContentText variant="h6" gutterBottom align="center">
+            <FormattedMessage
+              id="room.roomId"
+              defaultMessage="Room ID: {roomName}"
+              values={{
+                roomName: roomId,
+              }}
+            />
+          </DialogContentText>
+
+          <DialogContentText gutterBottom>
+            <FormattedMessage
+              id="room.setYourName"
+              defaultMessage={`Set your name for participation, 
+								and choose how you want to join:`}
+            />
+          </DialogContentText>
+
+          <TextField
+            id="displayname"
+            label={intl.formatMessage({
+              id: 'label.yourName',
+              defaultMessage: 'Your name',
+            })}
+            value={displayName}
+            variant="outlined"
+            margin="normal"
+            disabled={displayNameInProgress}
+            onChange={(event) => {
+              const { value } = event.target;
+
+              changeDisplayName(value);
+            }}
+            onKeyDown={handleKeyDown}
+            onBlur={() => {
+              if (displayName === '') changeDisplayName('Guest');
+              if (room.inLobby) roomClient.changeDisplayName(displayName);
+            }}
+            fullWidth
+          />
+          {!room.inLobby && room.overRoomLimit && (
+            <DialogContentText className={classes.red} variant="h6" gutterBottom>
+              <FormattedMessage id="room.overRoomLimit" defaultMessage={'The room is full, retry after some time.'} />
+            </DialogContentText>
+          )}
+        </DialogContent>
+
+        {!room.inLobby ? (
+          <DialogActions>
+            <Button
+              onClick={() => {
+                roomClient.join({ roomId, joinVideo: false });
+              }}
+              variant="contained"
+              color="secondary"
+            >
+              <FormattedMessage id="room.audioOnly" defaultMessage="Audio only" />
+            </Button>
+            <Button
+              onClick={() => {
+                roomClient.join({ roomId, joinVideo: true });
+              }}
+              variant="contained"
+              color="secondary"
+            >
+              <FormattedMessage id="room.audioVideo" defaultMessage="Audio and Video" />
+            </Button>
+          </DialogActions>
+        ) : (
+          <DialogContent>
+            <DialogContentText
+              className={classes.green}
+              gutterBottom
+              variant="h6"
+              style={{ fontWeight: '600' }}
+              align="center"
+            >
+              <FormattedMessage id="room.youAreReady" defaultMessage="Ok, you are ready" />
+            </DialogContentText>
+            {room.signInRequired ? (
+              <DialogContentText gutterBottom variant="h5" style={{ fontWeight: '600' }}>
+                <FormattedMessage
+                  id="room.emptyRequireLogin"
+                  defaultMessage={`The room is empty! You can Log In to start 
+										the meeting or wait until the host joins`}
+                />
+              </DialogContentText>
+            ) : (
+              <DialogContentText gutterBottom variant="h5" style={{ fontWeight: '600' }}>
+                <FormattedMessage
+                  id="room.locketWait"
+                  defaultMessage="The room is locked - hang on until somebody lets you in ..."
+                />
+              </DialogContentText>
+            )}
+          </DialogContent>
+        )}
+
+        {!isElectron() && (
+          <CookieConsent
+            buttonText={intl.formatMessage({
+              id: 'room.consentUnderstand',
+              defaultMessage: 'I understand',
+            })}
+          >
+            <FormattedMessage
+              id="room.cookieConsent"
+              defaultMessage="This website uses cookies to enhance the user experience"
+            />
+          </CookieConsent>
+        )}
+      </Dialog>
+    </div>
+  );
+};
+
+JoinDialog.propTypes = {
+  roomClient: PropTypes.any.isRequired,
+  room: PropTypes.object.isRequired,
+  roomId: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  displayNameInProgress: PropTypes.bool.isRequired,
+  loginEnabled: PropTypes.bool.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
+  myPicture: PropTypes.string,
+  changeDisplayName: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    room: state.room,
+    displayName: state.settings.displayName,
+    displayNameInProgress: state.me.displayNameInProgress,
+    loginEnabled: state.me.loginEnabled,
+    loggedIn: state.me.loggedIn,
+    myPicture: state.me.picture,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeDisplayName: (displayName) => {
+      dispatch(settingsActions.setDisplayName(displayName));
+    },
+  };
+};
+
+export default withRoomContext(
+  connect(mapStateToProps, mapDispatchToProps, null, {
+    areStatesEqual: (next, prev) => {
+      return (
+        prev.room.inLobby === next.room.inLobby &&
+        prev.room.signInRequired === next.room.signInRequired &&
+        prev.room.overRoomLimit === next.room.overRoomLimit &&
+        prev.settings.displayName === next.settings.displayName &&
+        prev.me.displayNameInProgress === next.me.displayNameInProgress &&
+        prev.me.loginEnabled === next.me.loginEnabled &&
+        prev.me.loggedIn === next.me.loggedIn &&
+        prev.me.picture === next.me.picture
+      );
+    },
+  })(withStyles(styles)(JoinDialog))
+);
---
+++ src/components/MeetingDrawer/Chat/Message.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import DOMPurify from 'dompurify';
+import marked from 'marked';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const linkRenderer = new marked.Renderer();
+
+linkRenderer.link = (href, title, text) => {
+  title = title ? title : href;
+  text = text ? text : href;
+
+  return `<a target='_blank' href='${href}' title='${title}'>${text}</a>`;
+};
+
+const styles = (theme) => ({
+  root: {
+    display: 'flex',
+    marginBottom: theme.spacing(1),
+    padding: theme.spacing(1),
+    flexShrink: 0,
+    boxShadow: 'none',
+    width: '78%',
+  },
+  selfMessage: {
+    marginLeft: 'auto',
+    justifyContent: 'flex-end',
+  },
+  remoteMessage: {
+    marginRight: 'auto',
+    justifyContent: 'flex-start',
+  },
+  info: {
+    alignSelf: 'flex-end',
+    marginRight: '8px',
+  },
+  text: {
+    alignSelf: 'flex-end',
+    marginRight: '8px',
+    '& p': {
+      margin: 0,
+    },
+  },
+  remoteText: {
+    alignSelf: 'flex-start',
+    '& p': {
+      margin: 0,
+    },
+  },
+  content: {
+    marginLeft: theme.spacing(1),
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  avatar: {
+    borderRadius: '4px',
+    height: '2rem',
+    alignSelf: 'end',
+  },
+});
+
+const Message = (props) => {
+  const { self, picture, text, time, name, classes } = props;
+  console.log(time);
+  return (
+    <Paper className={classnames(classes.root, self ? classes.selfMessage : classes.remoteMessage)}>
+      {self ? (
+        <React.Fragment>
+          <div className={classes.content}>
+            <Typography className={classes.info} variant="caption">
+              {time} - Me
+            </Typography>
+            <Typography
+              className={classes.text}
+              variant="subtitle1"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(marked.parse(text, { renderer: linkRenderer }), {
+                  ALLOWED_TAGS: ['a'],
+                  ALLOWED_ATTR: ['href', 'target', 'title'],
+                }),
+              }}
+            />
+          </div>
+          <img alt="Avatar" className={classes.avatar} src={picture} />
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <img alt="Avatar" className={classes.avatar} src={picture} />
+          <div className={classes.content}>
+            <Typography variant="caption">
+              {name} - {time}
+            </Typography>
+            <Typography
+              className={classes.remoteText}
+              variant="subtitle1"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(marked.parse(text, { renderer: linkRenderer }), {
+                  ALLOWED_TAGS: ['a'],
+                  ALLOWED_ATTR: ['href', 'target', 'title'],
+                }),
+              }}
+            />
+          </div>
+        </React.Fragment>
+      )}
+    </Paper>
+  );
+};
+
+Message.propTypes = {
+  self: PropTypes.bool,
+  picture: PropTypes.string,
+  text: PropTypes.string,
+  time: PropTypes.object,
+  name: PropTypes.string,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Message);
